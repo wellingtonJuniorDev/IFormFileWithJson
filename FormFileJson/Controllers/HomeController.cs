7395f6849b8506e2dbc2d277ac9f00ea957a083f
wellingtonJuniorDev <wellpod@hotmail.com>
2020-07-17 22:20:41 -0300

    init

---
+++ FormFileJson/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using FormFileJson.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace FormFileJson.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(CandidatoViewModel viewModel)
+        {            
+            //if(!ModelState.IsValid) { do something... }
+
+            var name = viewModel.Nome;
+            var imagemFormFile = viewModel.ImagemUpload;
+            // do something...
+
+            return new JsonResult(new { status = 200, message = "OK" });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FormFileJson/Models/CandidatoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FormFileJson.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormFileJson.Models
+{
+    [ModelBinder(typeof(FormFileWithJsonModelBinder), Name = "candidato")]
+    public class CandidatoViewModel
+    {        
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        
+        public IFormFile ImagemUpload { get; set; }
+    }
+}
